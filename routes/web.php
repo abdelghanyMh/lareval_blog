e07672b2b74e2f013b8493004ded356e791afca9
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-10-15 11:35:46 +0100

    add: Authentication Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,13 +27,12 @@ Route::get('/about', [HomeController::class, 'about'])->name('about');
  * php artisan route:list
  * https://laravel.com/docs/9.x/controllers#actions-handled-by-resource-controller
  */
-Route::resource('posts', PostController::class)->except(['index']);
+Route::resource('posts', PostController::class)->except(['index'])->middleware('auth');
 
 
-Route::match(['get', 'post'] , '/register', [AuthController::class, 'register'])->name('register');
-// Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register');
+Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
 
 
-Route::match(['get', 'post'] , '/login', [AuthController::class, 'login'])->name('login');
+Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
